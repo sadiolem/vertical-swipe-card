db160c0d31660be709943020e62ee1318435fced
i.tuev <sadiolem.sds@gmail.com>
2022-04-24 20:12:06 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SwipeCard />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import SwipeCard from './components/SwipeCard.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    SwipeCard,
   },
 };
 </script>
 
 <style lang="scss">
+html,
+body {
+  height: 100%;
+}
+
 #app {
+  position: relative;
+  overflow: hidden;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/SwipeCard.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="swipe-card" :style="{ top: getInitialTopPosition }">
+    <div
+      ref="card"
+      class="swipe-card-inner-wrapper"
+      @touchstart="handleTouchStart"
+    >
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Tempora numquam temporibus accusantium eum! Asperiores, quidem.
+        Voluptate harum ducimus delectus officia,
+        alias vitae doloribus praesentium ut sapiente officiis ea ad nihil.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Tempora numquam temporibus accusantium eum! Asperiores, quidem.
+        Voluptate harum ducimus delectus officia,
+        alias vitae doloribus praesentium ut sapiente officiis ea ad nihil.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Tempora numquam temporibus accusantium eum! Asperiores, quidem.
+        Voluptate harum ducimus delectus officia,
+        alias vitae doloribus praesentium ut sapiente officiis ea ad nihil.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SwipeCard',
+  data() {
+    return {
+      initialTouchStartPosition: 0,
+      touchStartPosition: 0,
+      touchCurrentPosition: 0,
+      initialPxValueOfVisibleCardPart: 250,
+      minPxValueOfVisibleCardPart: 100,
+    };
+  },
+  computed: {
+    card() {
+      return this.$refs.card;
+    },
+    getInitialTopPosition() {
+      return `calc(100% - ${this.initialPxValueOfVisibleCardPart}px)`;
+    },
+  },
+  methods: {
+    handleTouchStart(e) {
+      this.initialTouchStartPosition = e.touches[0].clientY;
+      this.touchStartPosition = e.touches[0].clientY - this.touchCurrentPosition;
+      this.card.addEventListener('touchmove', this.handleTouchMove, false);
+    },
+    handleTouchMove(e) {
+      this.touchCurrentPosition = e.changedTouches[0].clientY - this.touchStartPosition;
+
+      const isUpDirection = (this.initialTouchStartPosition - e.changedTouches[0].clientY) > 0;
+
+      if (isUpDirection) {
+        const isFullCardVisible = window.innerHeight
+          - this.card.getBoundingClientRect().bottom >= 0;
+
+        if (isFullCardVisible) {
+          // limit up swipe
+          this.touchCurrentPosition = -Math.abs(
+            this.card.getBoundingClientRect().bottom
+            - this.card.getBoundingClientRect().top
+            - this.initialPxValueOfVisibleCardPart,
+          );
+        }
+      } else {
+        const isMinPartOfCardIsVisible = window.innerHeight
+        - this.card.getBoundingClientRect().top
+        <= this.minPxValueOfVisibleCardPart;
+
+        if (isMinPartOfCardIsVisible) {
+          // limit down swipe
+          this.touchCurrentPosition = Math.abs(
+            window.innerHeight
+            - this.card.getBoundingClientRect().top
+            - this.initialPxValueOfVisibleCardPart,
+          );
+        }
+      }
+
+      this.card.style.transform = `translateY(${this.touchCurrentPosition}px)`;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.swipe-card {
+  position: absolute;
+  width: 100%;
+
+  .swipe-card-inner-wrapper {
+    position: absolute;
+    top: 0;
+    max-width: 100%;
+    padding: 24px;
+    border: 1px solid #000;
+    touch-action: none;
+  }
+}
+</style>
